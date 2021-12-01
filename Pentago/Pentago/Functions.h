f468f0bbdea58937c25be5a72243bffdb51138c5
aniabuto <aniabuto@wp.pl>
2021-12-01 21:24:37 +0100

    drawing a simple pentago board

--- Pentago/Pentago/Functions.h
+++ Pentago/Pentago/Functions.h
@@ -10,3 +10,19 @@ string GetName() {
 	return name;
 }
 
+void DrawBoard(char** board, int size) {
+	cout << "+---+---+---+  +---+---+---+\n";
+	for (int i = 0; i < size; i++)
+	{
+		cout << '|';
+		for (int j = 0; j < size; j++)
+		{
+			cout << ' ' << board[i][j] << " |";
+			if (j == 2)
+				cout << "  |";
+		}
+		cout << "\n+---+---+---+  +---+---+---+\n";
+		if (i == 2)
+			cout << "\n+---+---+---+  +---+---+---+\n";
+	}
+}
--- Pentago/Pentago/Source.cpp
+++ Pentago/Pentago/Source.cpp
@@ -6,12 +6,22 @@ using namespace std;
 int main()
 {
 	Player* one = new Player();
-	Player* two = new Player();
+	Player* two = new Player();/*
 	one->SetName(GetName());
 	two->SetName(GetName());
 
 	cout << "First player's name: " << one->GetName();
-	cout << "\nSecond player's name: " << two->GetName();
+	cout << "\nSecond player's name: " << two->GetName();*/
+	int size = 6;
+	char** board = new char*[size];
+	for (int i = 0; i < size; i++)
+		board[i] = new char[size];
+
+	for (int i = 0; i < size; i++)
+		for (int j = 0; j < size; j++)
+			board[i][j] = (i + j) % 2 == 0 ? 'o' : 'x';
+
+	DrawBoard(board, size);
 
 	return 0;
 }
